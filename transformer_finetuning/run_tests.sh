8eceb849f39dfa6f507274439101c743296f7a05
leba01 <124846286+leba01@users.noreply.github.com>
2024-03-14 20:49:15 -0700

    Update run_tests.sh

--- transformer_finetuning/run_tests.sh
+++ transformer_finetuning/run_tests.sh
@@ -5,6 +5,6 @@ peft=$2
 
 lm_eval --model hf \
   --model_args pretrained="$pretrained",peft="$peft" \
-  --tasks hellaswag, lambada_openai, arc_challenge, arc_easy, winogrande, piqa \
+  --tasks hellaswag,lambada_openai,arc_challenge,arc_easy,winogrande,piqa \
   --device cuda:0 \
   --batch_size 8
